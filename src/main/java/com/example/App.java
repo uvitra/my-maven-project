41fb08fb8a9b513094bb9036e77e194ca938de76
Global User <uvitra.contact@gmail.com>
2020-09-29 12:43:57 +0530

    Adding multiply support

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "2 X 3 = " + Util.multiply(2,3) );
     }
 }
---
+++ src/main/java/com/example/Util.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Util {
+
+    /**
+     * Multiplies two numbers
+     * @param a first number
+     * @param b first number
+     * @return int Result of a X b
+     */
+    public static int multiply(int a, int b) {
+        return a*b;
+    }
+}
